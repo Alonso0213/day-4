2cb839e9b9c5589f438b304f8dabb434a4c75c33
Alonso0213 <alonsocupido51@gmail.com>
2023-05-17 10:29:45 +0200

    arrays/objects

---
+++ script/code.js
@@ -0,0 +1,10 @@
+let person = {firstName:"John", lastName:"Doe", age:50,};
+console.log(typeof person);
+ 
+let person2 = {firstName:"Barry", lastName:"Allen", age:25,};
+console.log(typeof person2);
+
+let array=[]
+
+array.push( person,person2)
+console.log(array)
